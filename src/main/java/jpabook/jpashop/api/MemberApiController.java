531eecbc9df5b2bf969b77c1088a81807aaa99c3
hyun <caudeutsch@gmail.com>
2023-06-09 16:51:02 +0900

    회원조회 api 추가

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -8,12 +8,45 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    //가장 간단한 조회 api, 비추천
+    //1. 엔티티에 프레젠테이션 계층을 위한 로직이 추가됨(서로 분리되는 것이 베스트)
+    //2. 기본적으로 엔티티 모든 값이 노출 - 회원정보 패스워드같이 기밀성 요구되는 정보도 노출
+    //3. 응답 스펙을 맞추기 위해 로직이 추가 - 여러 다양한 응답 스펙이 있을 때 노답
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2(){
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+        return new Result(collect.size(), collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T>{
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
