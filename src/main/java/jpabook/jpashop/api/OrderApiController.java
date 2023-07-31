2657e0bd4803d8a448d60b3585ccc41090b0c303
hyun <caudeutsch@gmail.com>
2023-07-31 17:29:25 +0900

    컬렉션 조회 최적화

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -76,6 +76,13 @@ public class OrderApiController {
         return orderQueryRepository.findOrderQueryDtos();
     }
 
+
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5(){
+
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
     @Getter
     @Setter
     static class OrderDto {
